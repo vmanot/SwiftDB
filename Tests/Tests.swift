13ec926cac84cbe02bca60810b8d96c0892c6d32
Vatsal Manot <vatsal.manot@yahoo.com>
2021-09-11 17:14:38 +0530

    Update package

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -4,18 +4,46 @@
 
 @testable import SwiftDB
 
+import Filesystem
+import Combine
+import Merge
 import XCTest
 
-class Foo: Entity, Codable {
+class Foo: CustomStringConvertible, Entity, Codable, ObservableObject {
     @Attribute var x: Int = 0
+    
+    var description: String {
+        "Foo \(x)"
+    }
     
     required init() {
         
     }
 }
 
+struct TestSchema: Schema {
+    var body: Body {
+        Foo.self
+    }
+}
+
 final class SwiftDBTests: XCTestCase {
-    func testRuntime() {
-        try! JSONDecoder().decode(Foo.self, from: try! JSONEncoder().encode(Foo()))
+    func testRuntime() throws {
+        let database = try PersistentContainer(
+            name: "FooCoreDataDatabase",
+            schema: TestSchema(),
+            location: .filePath("/Users/vmanot/Desktop/Junk/SwiftDB-Tests/Test.sqlite")
+        )
+        
+        try database.load()
+        
+        let foo = try database.create(Foo.self)
+        
+        foo.x += 1
+        
+        try database.deleteAll()
+        try database.fetchAllInstances().printSelf()
+        
+        try database.save()
     }
 }
